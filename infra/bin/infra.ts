b8390d02b45b4c19110a07706bf05e96a3c6dde1
haandol <ldg55d@gmail.com>
2022-11-29 21:47:08 +0900

    add cdk8s

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,8 +2,28 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EksStack } from '../lib/stacks/eks-stack';
+import { VpcStack } from '../lib/stacks/vpc-stack';
 import { Config } from '../lib/configs/loader';
 
-const app = new cdk.App();
+const app = new cdk.App({
+  context: {
+    ns: Config.Ns,
+  },
+});
 
-new EksStack(app, `${Config.Ns}EksStack`, {});
+const vpcStack = new VpcStack(app, `${Config.Ns}VpcStack`, {
+  vpcId: Config.VpcId,
+  env: {
+    account: Config.AWS.Account,
+    region: Config.AWS.Region,
+  },
+});
+
+const eksStack = new EksStack(app, `${Config.Ns}EksStack`, {
+  vpc: vpcStack.vpc,
+  env: {
+    account: Config.AWS.Account,
+    region: Config.AWS.Region,
+  },
+});
+eksStack.addDependency(vpcStack);
--- infra/lib/stacks/eks-stack.ts
+++ infra/lib/stacks/eks-stack.ts
@@ -1,13 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk8s from 'cdk8s';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as eks from 'aws-cdk-lib/aws-eks';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { HelloChart } from '../charts/hello';
+
+interface IProps extends cdk.StackProps {
+  vpc: ec2.IVpc;
+}
 
 export class EksStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: IProps) {
     super(scope, id, props);
+
+    const ns = this.node.tryGetContext('ns');
+
+    const mastersRole = new iam.Role(this, 'cluster-master-role', {
+      assumedBy: new iam.AccountPrincipal(cdk.Stack.of(this).account),
+    });
 
     const cluster = new eks.FargateCluster(this, 'FargateCluster', {
       version: eks.KubernetesVersion.V1_23,
+      mastersRole,
+      clusterName: `${ns}fargate-cluster`,
+      outputClusterName: true,
+      outputConfigCommand: true,
+      outputMastersRoleArn: true,
+      endpointAccess: eks.EndpointAccess.PUBLIC_AND_PRIVATE,
+      vpc: props.vpc,
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
     });
+
+    const app = new cdk8s.App();
+    cluster.addCdk8sChart('hello', new HelloChart(app, 'hello'));
   }
 }
