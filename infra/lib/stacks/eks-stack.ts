0c994b8dc8938a60ff99e51d785c8233b0026da0
haandol <ldg55d@gmail.com>
2022-12-03 14:32:37 +0900

    use eks ec2 for using istio

--- infra/lib/stacks/eks-stack.ts
+++ infra/lib/stacks/eks-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as eks from 'aws-cdk-lib/aws-eks';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import { KubectlV23Layer } from '@aws-cdk/lambda-layer-kubectl-v23';
 
 interface IProps extends cdk.StackProps {
   vpc: ec2.IVpc;
@@ -14,34 +14,46 @@ export class EksStack extends cdk.Stack {
 
     const ns = this.node.tryGetContext('ns') as string;
 
-    const mastersRole = new iam.Role(this, 'cluster-master-role', {
-      assumedBy: new iam.AccountPrincipal(cdk.Stack.of(this).account),
-    });
-
-    const podExecutionRole = new iam.Role(this, 'PodExecutionRole', {
-      assumedBy: new iam.ServicePrincipal('eks-fargate-pods.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName(
-          'AmazonEKSFargatePodExecutionRolePolicy'
-        ),
-      ],
-    });
-
-    const cluster = new eks.FargateCluster(this, 'FargateCluster', {
-      version: eks.KubernetesVersion.V1_23,
-      mastersRole,
+    const cluster = new eks.Cluster(this, 'Cluster', {
+      vpc: props.vpc,
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
       clusterName: ns.toLowerCase(),
+      version: eks.KubernetesVersion.V1_23,
       outputClusterName: true,
       outputConfigCommand: true,
       outputMastersRoleArn: true,
       endpointAccess: eks.EndpointAccess.PUBLIC_AND_PRIVATE,
-      defaultProfile: {
-        fargateProfileName: `${ns.toLowerCase()}fargate-profile`,
-        podExecutionRole,
-        selectors: [{ namespace: 'default' }, { namespace: 'kube-system' }],
+      kubectlLayer: new KubectlV23Layer(this, 'kubectlV23Layer'),
+      defaultCapacity: 0,
+      defaultCapacityType: eks.DefaultCapacityType.EC2,
+      albController: {
+        version: eks.AlbControllerVersion.V2_4_1,
       },
-      vpc: props.vpc,
-      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
+    });
+
+    const launchTemplate = new ec2.LaunchTemplate(this, 'LaunchTemplate', {
+      launchTemplateName: ns.toLowerCase(),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.M5,
+        ec2.InstanceSize.LARGE
+      ),
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          mappingEnabled: true,
+          volume: ec2.BlockDeviceVolume.ebs(128, {
+            deleteOnTermination: true,
+            volumeType: ec2.EbsDeviceVolumeType.GP2,
+            encrypted: true,
+          }),
+        },
+      ],
+    });
+    cluster.addNodegroupCapacity('CustomNodeGroup', {
+      launchTemplateSpec: {
+        id: launchTemplate.launchTemplateId!,
+        version: launchTemplate.versionNumber!,
+      },
     });
   }
 }
